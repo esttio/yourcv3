38f34213f994c002fc93575b68b2127264e03acc
Estio Nurcahyanto <esttio2@gmail.com>
2019-12-25 14:46:32 +0700

    Updates

---
+++ precache-manifest.78698bebf86f892dfa9f35071f3faa88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ec28622d48c745657cff706b8d4e00",
+    "url": "/yourcv3/index.html"
+  },
+  {
+    "revision": "3daa42caafdcbe68ef2b",
+    "url": "/yourcv3/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "c19068765723c7837df1",
+    "url": "/yourcv3/static/css/main.9fa1cc5c.chunk.css"
+  },
+  {
+    "revision": "3daa42caafdcbe68ef2b",
+    "url": "/yourcv3/static/js/2.da86489e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/yourcv3/static/js/2.da86489e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c19068765723c7837df1",
+    "url": "/yourcv3/static/js/main.ef485938.chunk.js"
+  },
+  {
+    "revision": "ab51dbeef6f438b45911",
+    "url": "/yourcv3/static/js/runtime-main.d7790c19.js"
+  },
+  {
+    "revision": "7ae75c6c2b02fb83df08b46f3c5aab87",
+    "url": "/yourcv3/static/media/button-bg1.7ae75c6c.png"
+  },
+  {
+    "revision": "4d539ec68f9088bd2f48c0c8d3957aef",
+    "url": "/yourcv3/static/media/icon1.4d539ec6.svg"
+  },
+  {
+    "revision": "b737f3b2f613a4cd2617b97873b1bac8",
+    "url": "/yourcv3/static/media/icon2.b737f3b2.svg"
+  },
+  {
+    "revision": "595d43afd13939140b563ac1cdfb504b",
+    "url": "/yourcv3/static/media/icon5.595d43af.svg"
+  },
+  {
+    "revision": "bb52a332b105ac1433337a030b27b026",
+    "url": "/yourcv3/static/media/img1.bb52a332.svg"
+  },
+  {
+    "revision": "f593de9282d1d9f512fae082320c7cf0",
+    "url": "/yourcv3/static/media/logo.f593de92.svg"
+  }
+]);
